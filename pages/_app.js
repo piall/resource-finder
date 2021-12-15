dd0188a692cf0c4c4395a1c7bffa7f9f4bb539c5
Md. Pial Ahamed <mdpialahamed@gmail.com>
2021-12-15 09:30:51 +0600

    Add custom theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.scss';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffd803',
+    },
+    secondary: {
+      main: '#E33E7F',
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </MuiThemeProvider>
+  );
 }
 
 export default MyApp;
