64e715c7c3600fc5541cb201bdeb3ddd833290fb
Md. Pial Ahamed <mdpialahamed@gmail.com>
2021-12-19 14:01:30 +0600

    Add public footer and navbar components

--- src/components/public/Navbar.jsx
+++ src/components/public/Navbar.jsx
@@ -1,15 +1,42 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import Button from '@material-ui/core/Button';
 
 export default function Navbar() {
+  const router = useRouter();
+
   return (
     <div>
       <nav className="public">
         <p className="brand">
-          <Link href="/">Resource Finder</Link>
+          <Link href="/">
+            <img src="/logo.svg" />
+          </Link>
         </p>
-        <button className="login-btn">
-          <Link href="/login">Login</Link>
-        </button>
+        <div className="links-container">
+          <Link
+            href="#Home"
+            className={router.asPath == '/#Home' ? 'active' : ''}
+          >
+            Home
+          </Link>
+
+          <Link
+            href="#About"
+            className={router.asPath == '/#About' ? 'active' : ''}
+          >
+            About
+          </Link>
+          <Link
+            href="#Topics"
+            className={router.asPath == '/#Topics' ? 'active' : ''}
+          >
+            Topics
+          </Link>
+        </div>
+        <Button variant="contained" color="primary">
+          <Link href="/siginin">Sign In</Link>
+        </Button>
       </nav>
     </div>
   );
