799be7aa4e7e0410e7ce8d9ecf72f41e18b82932
Md. Pial Ahamed <mdpialahamed@gmail.com>
2021-12-26 11:15:25 +0600

    Add navbar for admin

---
+++ src/components/private/admin/Navbar.jsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import Button from '@material-ui/core/Button';
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="public">
+        <p className="brand">
+          <Link href="/admin/home">
+            <img src="/logo.svg" />
+          </Link>
+        </p>
+        <div className="links-container">
+          <Link href="/admin/home">Home</Link>
+          <Link href="/admin/topic">Topic</Link>
+          <Link href="/admin/resource">Resource</Link>
+          <Link href="/admin/author">Author</Link>
+        </div>
+        <Button variant="contained" color="primary">
+          Sign Out
+        </Button>
+      </nav>
+    </div>
+  );
+}
