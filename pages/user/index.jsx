a48a63aa6b5570f3bda2651a6f744096606128c5
Md. Pial Ahamed <mdpialahamed@gmail.com>
2021-12-28 16:09:28 +0600

    Add topics

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -1,7 +1,20 @@
+import Link from 'next/link';
+import { Grid } from '@material-ui/core';
+
+import Navbar from '../../src/components/private/user/Navbar';
+import Topics from '../../src/components/private/topic/Topics';
+
 export default function Home() {
   return (
-    <div className="page-container-scroll">
-      <div className="page-container"></div>
-    </div>
+    <>
+      <Navbar />
+      <div className="page-container-scroll">
+        <div className="page-container">
+          <div className="center-container">
+            <Topics />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- pages/user/topic/index.jsx
+++ pages/user/topic/index.jsx
@@ -1,7 +1,12 @@
+import Topics from '../../../src/components/private/topic/Topics';
+
 export default function Topic() {
   return (
     <div className="page-container-scroll">
-      <div className="page-container"></div>
+      <div className="page-container">
+        <h2 className="page-title">Topics</h2>
+        <Topics />
+      </div>
     </div>
   );
 }
