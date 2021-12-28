98b25c8759c12cbb74a760243c2f0083f1017100
Md. Pial Ahamed <mdpialahamed@gmail.com>
2021-12-28 10:01:32 +0600

    Add add author modal

--- pages/user/author/index.jsx
+++ pages/user/author/index.jsx
@@ -1,7 +1,57 @@
+import { useState } from 'react';
+import { Button, Modal, TextField } from '@material-ui/core';
+import { HiPlus, HiPencil, HiOutlineTrash } from 'react-icons/hi';
+
 export default function Topic() {
+  //modal state
+  const [addAuthorOpen, setAddAuthorOpen] = useState(false);
+  const [voteOpen, setVoteOpen] = useState(false);
+
+  //modal handler
+  const handleAddAuthorModal = () => {
+    setAddAuthorOpen(!addAuthorOpen);
+  };
+
   return (
     <div className="page-container-scroll">
-      <div className="page-container"></div>
+      <div className="page-container">
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<HiPlus />}
+          onClick={handleAddAuthorModal}
+        >
+          Add Author
+        </Button>
+
+        <Modal
+          open={addAuthorOpen}
+          onClose={handleAddAuthorModal}
+          aria-labelledby="add-author-modal"
+        >
+          <div className="modal-container">
+            <div className="modal-body">
+              <TextField
+                required
+                fullWidth
+                label="GitHub Handle"
+                placeholder="Enter GitHub Handle"
+                variant="outlined"
+              />
+              <center>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  startIcon={<HiPlus />}
+                  onClick={handleAddAuthorModal}
+                >
+                  Add
+                </Button>
+              </center>
+            </div>
+          </div>
+        </Modal>
+      </div>
     </div>
   );
 }
