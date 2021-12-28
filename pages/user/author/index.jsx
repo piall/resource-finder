961d624b1560c94f1c3478c0e1c0ebd51c40b06e
Md. Pial Ahamed <mdpialahamed@gmail.com>
2021-12-28 10:48:46 +0600

    Add author page

--- pages/user/author/index.jsx
+++ pages/user/author/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, Modal, TextField } from '@material-ui/core';
+import { Grid, Button, Modal, TextField } from '@material-ui/core';
 import { HiPlus, HiPencil, HiOutlineTrash } from 'react-icons/hi';
 
 export default function Topic() {
@@ -15,14 +15,52 @@ export default function Topic() {
   return (
     <div className="page-container-scroll">
       <div className="page-container">
-        <Button
-          variant="contained"
-          color="primary"
-          startIcon={<HiPlus />}
-          onClick={handleAddAuthorModal}
-        >
-          Add Author
-        </Button>
+        <div className="btn-container">
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<HiPlus />}
+            onClick={handleAddAuthorModal}
+          >
+            Add Author
+          </Button>
+          <Button
+            variant="contained"
+            className="danger"
+            startIcon={<HiOutlineTrash />}
+          >
+            Delete Author
+          </Button>
+        </div>
+
+        <div className="authors-container">
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={4}>
+              <div className="author-container">
+                <img src="https://v4.mui.com/static/ads-in-house/tidelift.png" />
+                <h2>Md. Pial Ahamed</h2>
+              </div>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <div className="author-container">
+                <img src="https://v4.mui.com/static/ads-in-house/tidelift.png" />
+                <h2>Md. Pial Ahamed</h2>
+              </div>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <div className="author-container">
+                <img src="https://v4.mui.com/static/ads-in-house/tidelift.png" />
+                <h2>Md. Pial Ahamed</h2>
+              </div>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <div className="author-container">
+                <img src="https://v4.mui.com/static/ads-in-house/tidelift.png" />
+                <h2>Md. Pial Ahamed</h2>
+              </div>
+            </Grid>
+          </Grid>
+        </div>
 
         <Modal
           open={addAuthorOpen}
