1d34a93c64d89e14cf57265260b9fc0bfbdc3bd0
Md. Pial Ahamed <mdpialahamed@gmail.com>
2021-12-29 08:55:04 +0600

    Wrap with user layout component

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -1,13 +1,9 @@
-import Link from 'next/link';
-import { Grid } from '@material-ui/core';
-
-import Navbar from '../../src/components/private/user/Navbar';
+import UserLayout from '../../src/components/layout/UserLayout';
 import Topics from '../../src/components/private/topic/Topics';
 
 export default function Home() {
   return (
-    <>
-      <Navbar />
+    <UserLayout>
       <div className="page-container-scroll">
         <div className="page-container">
           <div className="center-container">
@@ -15,6 +11,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-    </>
+    </UserLayout>
   );
 }
