b3c036a99e3e24a791def62abb88c0ac19540f62
Md. Pial Ahamed <mdpialahamed@gmail.com>
2022-01-17 08:27:24 +0600

    Update resource model

--- backend/model/resource.js
+++ backend/model/resource.js
@@ -2,27 +2,21 @@ const mongoose = require('mongoose');
 const voteSchema = require('./vote');
 
 const resourceSchema = new mongoose.Schema({
-  resourceID: {
-    type: String,
-    unique: true,
-    immutable: true,
-  },
-  topicID: {
-    type: String,
-    immutable: true,
-  },
   link: {
     type: String,
     required: true,
   },
-  addedBy: {
-    type: String,
-    unique: true,
-    immutable: true,
-  },
   title: String,
   description: String,
   vote: voteSchema,
+  topicID: {
+    type: mongoose.Types.ObjectId,
+  },
+  userID: {
+    type: mongoose.Types.ObjectId,
+    // unique: true,
+    // immutable: true,
+  },
 });
 
 module.exports = mongoose.model('resource', resourceSchema);
