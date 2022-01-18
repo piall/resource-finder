c93c83377028a1a83e6260f4d090a9770f721156
Md. Pial Ahamed <mdpialahamed@gmail.com>
2022-01-18 10:43:26 +0600

    Add signup signin API

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const SignUp = require('./methods/signup');
+const SignIn = require('./methods/signin');
 const Resource = require('./methods/resource');
 
 const app = express();
 
-//middleware
 app.use(express.json());
 app.use(cors());
 
@@ -14,7 +15,9 @@ mongoose.connect(
   'mongodb+srv://pial:resource-finder@resource-finder.4ck71.mongodb.net/resource-finder?retryWrites=true&w=majority'
 );
 
-//API - Resource
+app.post('/api/signup', SignUp);
+app.post('/api/signin', SignIn);
+
 app.post('/api/user/resource/add', Resource.addResource);
 
 PORT = process.env.PORT || 5000;
