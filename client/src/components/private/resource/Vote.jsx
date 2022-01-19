5dc47e0cf4ef641d0d77d1c4aeb06ea16b96a949
Md. Pial Ahamed <mdpialahamed@gmail.com>
2022-01-19 16:42:18 +0600

    Update vote component

--- client/src/components/private/resource/Vote.jsx
+++ client/src/components/private/resource/Vote.jsx
@@ -1,19 +1,27 @@
 import { Button } from '@material-ui/core';
 import { MdHowToVote } from 'react-icons/md';
 
-export default function Vote({ handleVoteModal, voteType, voteCount, topic }) {
+export default function Vote({
+  handleVoteModal,
+  voteType,
+  voteCount,
+  topic,
+  resourceID,
+}) {
   return (
     <div className="resource-left-container">
       <Button
         variant="contained"
         color="primary"
         startIcon={<MdHowToVote />}
-        onClick={handleVoteModal}
+        onClick={() => {
+          handleVoteModal(resourceID);
+        }}
       >
         Vote
       </Button>
       <p>
-        {voteType} ({voteCount})
+        {voteType} {voteCount !== 0 ? '(' + voteCount + ')' : ''}
       </p>
       <p>{topic}</p>
     </div>
